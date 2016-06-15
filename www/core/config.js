6ed761394bc466744cff0db45aa313e711cdb746
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-06-15 08:27:08 +0200

    Changed the theme of the application

--- www/core/config.js
+++ www/core/config.js
@@ -7,21 +7,9 @@
     function ThemingProvider($mdThemingProvider,$mdIconProvider) {
 
 
- $mdThemingProvider.theme("default").primaryColor("blue-grey").accentColor("red");
-    //     var customBlueMap = $mdThemingProvider.extendPalette('black', {
-    //         'contrastDefaultColor': 'light',
-    //         'contrastDarkColors': ['50'],
-    //         '50': 'ffffff'
-    //     });
-    //     $mdThemingProvider.definePalette('customBlue', customBlueMap);
-    //     $mdThemingProvider.theme('default')
-    //         .primaryPalette('customBlue', {
-    //             'default': '500',
-    //             'hue-1': '50'
-    //         })
-    //         .accentPalette('pink');
-    //     $mdThemingProvider.theme('input', 'default')
-    //         .primaryPalette('grey');
+ $mdThemingProvider.theme("default")
+ .primaryPalette("indigo")
+
    }
 
 })();
