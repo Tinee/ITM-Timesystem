9dd4f3385eded3135927730f37b72a0b97a229ea
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-06-20 09:39:06 +0200

    Fixed a isLoggedIn property, that is going to be the lookup property to check if someone is loggedin, and a logout property.

--- www/layout/layout.js
+++ www/layout/layout.js
@@ -5,17 +5,22 @@
     .module('app.layout')
     .controller('LayoutController', LayoutController);
 
-  LayoutController.$inject = ['$scope', '$mdSidenav', 'menuService', 'changeView'];
-  function LayoutController($scope, $mdSidenav, menuService, changeView) {
+  LayoutController.$inject = ['$scope', '$mdSidenav', 'menuService', 'changeView','authService'];
+  function LayoutController($scope, $mdSidenav, menuService, changeView,authService) {
     var vm = this;
 
     vm.toggleSidenav = toogleSidenav
     vm.menuItems = menuService;
     vm.changeView = changeView.goToo;
+    vm.logout = authService.logout;
+    
+    vm.isLoggedIn = authService.checkLoggedInStatus()
+    
 
     function toogleSidenav(menuId) {
       $mdSidenav(menuId).toggle();
     };
+
 
   }
 })();
