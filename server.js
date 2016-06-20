9d92756b3f61e0c5e84622e304cffe415056511f
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-06-20 16:09:04 +0200

    Added so it connects to mongoSecondaryMongoDb and added Access-Control-Allow-Orgin allowed for all.

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 var express = require('express');
 var app = express();
 
-
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/Favorite');
+mongoose.connect('mongodb://localhost/SecondaryMongoDb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-   extended: true
+    extended: true
 }));
 
-var cats = require('./server/routes/favorite.route.js')(app);
+app.use(function (req, res, next) {
 
-var server = app.listen(3000,function(){
-   console.log('Server running at http://127.0.0.1:3000/');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+ require('./server/routes/bookmarks.route.js')(app);
+
+var server = app.listen(3000, function () {
+    console.log('Server running at http://127.0.0.1:3000/');
 });
