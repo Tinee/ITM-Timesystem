da9b0d0f65aa4c15903b201d0e8ff054b331b5df
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-06-20 08:23:35 +0200

    Added authInterceptorService, it catches a event for every http request and put in a token if we have it.

--- www/core/config.js
+++ www/core/config.js
@@ -1,17 +1,20 @@
 (function () {
   'use strict';
 
-  angular.module('app.core').config(ThemingProvider);
+  angular.module('app.core').config(Config);
 
-  ThemingProvider.$inject = ['$mdThemingProvider'];
+  Config.$inject = ['$mdThemingProvider','$httpProvider'];
+  function Config($mdThemingProvider,$httpProvider ) {
 
-  function ThemingProvider($mdThemingProvider) {
-
+    //Themeconfig to change colors on the client.
     $mdThemingProvider.theme('default')
       .primaryPalette('blue-grey')
       .accentPalette('amber')
       .warnPalette('red')
       .backgroundPalette('grey');
-  }
 
+    //Authinterceptor bootstrapping.
+$httpProvider.interceptors.push('authInterceptorService');
+        $httpProvider.defaults.headers.post = { 'Content-Type': 'application/json' };
+  }
 })();
