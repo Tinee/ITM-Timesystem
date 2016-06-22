c6fb5fb90fb3e199b3797530a3673613ea94b07f
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-06-22 08:27:03 +0200

    Directive for myEnter, triggers an event when pressing enter.

---
+++ www/directives/myEnter.js
@@ -0,0 +1,25 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.directives')
+        .directive('myEnter', MyEnter);
+
+    MyEnter.$inject = [];
+    function MyEnter() {
+        return function (scope, element, attrs) {
+            element.bind("keydown keypress", function (event) {
+                if (event.which === 13) {
+                    scope.$apply(function () {
+                        scope.$eval(attrs.myEnter);
+                    });
+                    event.preventDefault();
+                }
+            });
+        };
+    }
+})();
+
+
+
+
