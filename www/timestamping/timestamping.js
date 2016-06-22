761d74d2e93277dc7985fc348cf84c341ba6dda0
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-06-22 12:14:00 +0200

    Added some logic for the search function.

--- www/timestamping/timestamping.js
+++ www/timestamping/timestamping.js
@@ -5,8 +5,8 @@
         .module('app.timestamping')
         .controller('TimestampingController', TimestampingController);
 
-    TimestampingController.$inject = ['$scope', '$filter', '$mdpTimePicker', '$mdDialog', 'dataService'];
-    function TimestampingController($scope, $filter, $mdpTimePicker, $mdDialog, dataService) {
+    TimestampingController.$inject = ['$scope', '$filter', '$mdpTimePicker', '$mdDialog', 'dataService', '$q', '$timeout'];
+    function TimestampingController($scope, $filter, $mdpTimePicker, $mdDialog, dataService, $q, $timeout) {
 
         var vm = this;
 
@@ -15,19 +15,41 @@
         vm.selectedDate;
         vm.searchText = "";
 
-        vm.dayClick = function (date) {
+        vm.selectedItem = null;
+        vm.searchText = null;
+
+        vm.showConfirm = showConfirm;
+        vm.querySearch = querySearch;
+        vm.dayClick = dayClick;
+        vm.setDayContent = setDayContent;
+        
+
+        function dayClick(date) {
 
             if (date === undefined) return;
             vm.readableDate = $filter("date")(date, "MMMM d");
             vm.selectedDate = date;
         };
 
-        vm.setDayContent = function (date) {
+        function setDayContent(date) {
             return "<p></p>";
         };
 
-        vm.showConfirm = function (ev) {
-            // Appending dialog to document.body to cover sidenav in docs app
+        function querySearch(query) {
+            var results = query ? vm.bookmarks.filter(createFilterFor(query)) : vm.bookmarks;
+            var deferred = $q.defer();
+            $timeout(function () { deferred.resolve(results); }, Math.random() * 1000, false);
+            return deferred.promise;
+        }
+
+        function createFilterFor(query) {
+            return function filterFn(state) {
+                var p = state.bookmarkTitle.indexOf(query)
+                return (p === 0);
+            };
+        }
+
+        function showConfirm(ev) {
             var confirm = $mdDialog.confirm()
                 .title('Spara bokmärke')
                 .textContent('Fyll i uppgifterna nedan.')
