a7199ac8f79cd830dd751d37f924b2830bdc9f87
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-06-27 10:47:04 +0200

    Added some test code.

--- www/auth/authInterceptorService.js
+++ www/auth/authInterceptorService.js
@@ -5,12 +5,12 @@
         .module('app.auth')
         .factory('authInterceptorService', authInterceptorService);
 
-    authInterceptorService.$inject = ['$q', '$location'];
-    function authInterceptorService($q, $location) {
+    authInterceptorService.$inject = ['$q', '$location','localstorageFactory'];
+    function authInterceptorService($q, $location,localstorageFactory) {
 
         var services = {
             request: request,
-            responseError:responseError
+            responseError: responseError
         };
 
         return services;
@@ -19,13 +19,25 @@
 
             config.headers = config.headers || {};
 
-       
+
+            var user = localstorageFactory.get('user');
+
+            if (user) {
+                config.headers.Authorization = 'Bearer ' + user.token;
+            }
+            else {
+                if ($location.path() !== '/Inlogg') {
+                    $location.path('/Inlogg');
+                }
+
+            }
+
             return config;
         };
 
         function responseError(rejection) {
             if (rejection.status === 401) {
-                $location.path('/login');
+                $location.path('/Inlogg');
             }
             return $q.reject(rejection);
         };
