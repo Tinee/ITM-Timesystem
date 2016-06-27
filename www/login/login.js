1c65f53b07457fbafb27e9f59c91880fa8e3a84b
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-06-27 10:47:56 +0200

    Added a login function.

--- www/login/login.js
+++ www/login/login.js
@@ -1,12 +1,23 @@
-(function() {
-'use strict';
+(function () {
+    'use strict';
 
     angular
         .module('app.login')
         .controller('LoginController', LoginController);
 
-    LoginController.$inject = [];
-    function LoginController() {
+    LoginController.$inject = ['authService'];
+    function LoginController(authService) {
         var vm = this;
+
+        vm.loginData = {
+            username: "",
+            password: ""
+        }
+
+        vm.login = login;
+
+        function login(loginData) {
+            authService.login(loginData);
+        }
     }
 })();
