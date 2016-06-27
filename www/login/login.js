44d2d650e961a31bc5b98a637e57a6fd58d7000a
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-06-27 16:04:48 +0200

    Made some promise functions in login.

--- www/login/login.js
+++ www/login/login.js
@@ -5,8 +5,8 @@
         .module('app.login')
         .controller('LoginController', LoginController);
 
-    LoginController.$inject = ['authService'];
-    function LoginController(authService) {
+    LoginController.$inject = ['authService', '$mdToast', '$log', 'localstorageFactory', '$state'];
+    function LoginController(authService, $mdToast, $log, localstorageFactory, $state) {
         var vm = this;
 
         vm.loginData = {
@@ -17,7 +17,20 @@
         vm.login = login;
 
         function login(loginData) {
-            authService.login(loginData);
+            authService.login(loginData).then(function (res) {
+                localstorageFactory.set('user', res)
+                $state.go('layout.timestamping')
+
+            }, function (err) {
+                
+                $mdToast.show(
+                    $mdToast.simple()
+                        .textContent('Fel vid inloggningen')
+                        .position('top right')
+                        .hideDelay(3000)
+                );
+                $log.warn(err);
+            });
         }
     }
 })();
