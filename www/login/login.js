ece9edf56f2252fb076b7aa733e28b25630d04ef
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-06-29 14:54:08 +0200

    Login now shouts to parent to say we are online.

--- www/login/login.js
+++ www/login/login.js
@@ -5,8 +5,8 @@
         .module('app.login')
         .controller('LoginController', LoginController);
 
-    LoginController.$inject = ['authService', '$mdToast', '$log', 'localstorageFactory', '$state'];
-    function LoginController(authService, $mdToast, $log, localstorageFactory, $state) {
+    LoginController.$inject = ['$scope','authService', '$mdToast', '$log', 'localstorageFactory', '$state'];
+    function LoginController($scope,authService, $mdToast, $log, localstorageFactory, $state) {
         var vm = this;
 
         vm.loginData = {
@@ -15,12 +15,13 @@
         }
 
         particlesJS.load('particles-js', '../particles.json');
-        
+
         vm.login = login;
 
         function login(loginData) {
             authService.login(loginData).then(function (res) {
                 localstorageFactory.set('user', res)
+                $scope.$parent.isLoggedIn = true;
                 $state.go('layout.timestamping')
 
             }, function (err) {
